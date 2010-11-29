f2dc088687e5913a063b78d8eb5592d1ae4cf62b
xZise <CommodoreFabianus@gmx.de>
2010-11-29 16:33:11 +0100

    *Import new interface files from offical svn repository (including license header);

--- src/edu/kit/iti/iface/IServer.java
+++ src/edu/kit/iti/iface/IServer.java
@@ -1,3 +1,20 @@
+/*  Copyright (C) 2010  Karlsruhe Institute of Technology
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
 package edu.kit.iti.pse.iface;
 
 /**
@@ -93,9 +110,16 @@ public interface IServer {
     public int getOwner (int position);
     
     /**
-     * Liefert den aktuellen Wert der Würfel.
+     * Liefert den aktuellen Wert (Summe) der Würfel.
      */
     public int getDiceValue ();
+    
+    /**
+     * Liefert die Werte der <i>einzelnen</i> Würfel.
+     * @return jeweils einzelne Würfelwerte auf den Arraypositionen 0 und 1
+     * @since SVN revision 7
+     */
+    public int[] getDiceValues ();
     
     /**
      * Liefert das aktuelle Barvermögen eines Spielers.
