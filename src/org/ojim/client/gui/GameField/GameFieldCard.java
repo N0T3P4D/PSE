2b98378406344de2e0b0bf5d581ea578035470e6
DuKeTHeReaL <herzog-max@gmx.de>
2011-01-10 20:03:27 +0100

    GUI Update

--- src/org/ojim/client/gui/GameField/GameFieldCard.java
+++ src/org/ojim/client/gui/GameField/GameFieldCard.java
@@ -3,13 +3,14 @@ package org.ojim.client.gui.GameField;
 import java.awt.Image;
 
 @SuppressWarnings("serial")
-public class GameFieldCard implements GameFieldPiece {
+public class GameFieldCard extends GameFieldPiece {
 
 	private boolean turnedAround = false;
 
 	// FIXME: cardID statt ID Objekt!
 	public GameFieldCard(int cardId, String name, int position, Image image,
 			int price) {
+		super(price, name, price, image);
 		// TODO Auto-generated constructor stub
 	}
 
