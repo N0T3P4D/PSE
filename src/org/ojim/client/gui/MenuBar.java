11719d373a9696f0dd3c598b715bbff752ac4e49
DuKeTHeReaL <herzog-max@gmx.de>
2011-01-11 13:40:24 +0100

    kleiner GUI Fix

--- src/org/ojim/client/gui/MenuBar.java
+++ src/org/ojim/client/gui/MenuBar.java
@@ -16,9 +16,9 @@ public class MenuBar extends JMenuBar {
 	private JMenu fileMenu;
 	private JMenu editMenu;
 	private JMenuItem createGame;
-	private JMenuItem joinGame;
+	private JMenu joinGame;
 	private JMenuItem leaveGame;
-	private JMenu settings;
+	private JMenuItem settings;
 	private JMenuItem directConnection;
 	private JMenuItem serverList;
 	private JMenuItem exit;
@@ -38,9 +38,9 @@ public class MenuBar extends JMenuBar {
         editMenu = new JMenu(language.getText("?"));
 
         createGame = new JMenuItem(language.getText("Spiel erstellen"));
-        joinGame = new JMenuItem(language.getText("Spiel beitreten"));
+        joinGame = new JMenu(language.getText("Spiel beitreten"));
         leaveGame = new JMenuItem(language.getText("Spiel verlassen"));
-        settings = new JMenu(language.getText("Einstellungen"));
+        settings = new JMenuItem(language.getText("Einstellungen"));
 
         directConnection = new JMenuItem(language.getText("Direkte Verbindung"));
         serverList = new JMenuItem(language.getText("Serverliste"));
@@ -76,8 +76,8 @@ public class MenuBar extends JMenuBar {
         fileMenu.add(settings);
         fileMenu.add(exit);
 
-        settings.add(directConnection);
-        settings.add(serverList);
+        joinGame.add(directConnection);
+        joinGame.add(serverList);
         
         editMenu.add(about);
         editMenu.add(help);
