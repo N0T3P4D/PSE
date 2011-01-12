20cb5e50d07306b7615fe20c391870c13aefcf7d
Usman Ahmed <Leopardosz@web.de>
2011-01-12 18:49:37 +0100

    Update

---
+++ src/org/ojim/network/ClientDetails.java
@@ -0,0 +1,14 @@
+package network;
+
+public class ClientDetails {
+	
+	private String ip;
+	
+	private String username;
+	
+	public ClientDetails(String ip,String username){
+		this.ip = ip;
+		this.username = username;
+	}
+
+}
