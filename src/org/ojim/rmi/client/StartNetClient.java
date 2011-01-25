eb2d7336085ad907a03767d196f0ade36b8d73af
Usman Ahmed <Leopardosz@web.de>
2011-01-25 23:11:48 +0100

    Updates ,Es sind zwei Klassen hinzugekommen StartNetClient.java StartNetOjim.java

---
+++ src/org/ojim/rmi/client/StartNetClient.java
@@ -0,0 +1,48 @@
+/*  Copyright (C) 2010 - 2011  Fabian Neundorf, Philip Caroli,
+ *  Maximilian Madlung,	Usman Ghani Ahmed, Jeremias Mechler
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.ojim.rmi.client;
+
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import org.ojim.rmi.server.NetOjim;
+
+public class StartNetClient {
+	
+	public void createClientRMIConnection(int portNum,String ip, NetClient client){
+		 
+		String registryURL = "rmi://"+ip+":" + portNum + "/myServer"; 
+		 try {
+			
+			NetOjim server =(NetOjim)Naming.lookup(registryURL);
+			//ClientBase base = null ;
+			//NetClient clientInter = new ImplNetClient(base);
+			server.registerClient(client);
+			System.out.println("Client wurde beim Server angemeldet");
+			
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		} catch (NotBoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+
+}
