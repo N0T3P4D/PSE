786ee74e8a8cd755f579110b9f85dcfacf325eec
xZise <CommodoreFabinus@gmx.de>
2011-01-25 16:22:18 +0100

    *first wrapper idea;

--- src/org/ojim/rmi/client/ImplNetClient.java
+++ src/org/ojim/rmi/client/ImplNetClient.java
@@ -7,9 +7,13 @@ import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
 import org.ojim.client.ClientBase;
+import org.ojim.iface.IClient;
+import org.ojim.iface.Rules;
 import org.ojim.network.ClientDetails;
 import org.ojim.rmi.server.ImplNetOjim;
 import org.ojim.rmi.server.NetOjim;
+
+import edu.kit.iti.pse.iface.IServer;
 
 /**
  * Klasse verwaltet den Verbindungsaufbau vom Client zum Server.
@@ -17,11 +21,13 @@ import org.ojim.rmi.server.NetOjim;
  * @author Usman Ghani Ahmed
  *
  */
-public class ImplNetClient extends UnicastRemoteObject implements NetClient {
+public class ImplNetClient extends UnicastRemoteObject implements NetClient, IServer {
 	
 	ClientBase clientBase;
 	
 	ClientDetails details;
+	
+	NetOjim server;
 	
 	public ImplNetClient(ClientBase clientBase, ClientDetails details) throws RemoteException {
 		super();
@@ -30,14 +36,13 @@ public class ImplNetClient extends UnicastRemoteObject implements NetClient {
 		
 	}
 	
-	public NetOjim createClientRMIConnection(int portNum,String ip){
+	public void createClientRMIConnection(int portNum,String ip){
 		 
-		NetOjim serverOjim = null;
 		String registryURL = "rmi://"+ip+":" + portNum + "/myServer"; 
 		 try {
-			serverOjim =(NetOjim)Naming.lookup(registryURL);
+			this.server =(NetOjim)Naming.lookup(registryURL);
 			NetClient clientInter = this;
-			serverOjim.registerClient(clientInter);
+			this.server.registerClient(clientInter);
 			System.out.println("Client wurde beim Server angemeldet");
 			
 		} catch (MalformedURLException e) {
@@ -47,11 +52,7 @@ public class ImplNetClient extends UnicastRemoteObject implements NetClient {
 		} catch (NotBoundException e) {
 			e.printStackTrace();
 		}
-		
-		return serverOjim;
 	}
-	
-	
 	
 	
 
@@ -145,6 +146,233 @@ public class ImplNetClient extends UnicastRemoteObject implements NetClient {
 	@Override
 	public void informMove(int playerId, int position) {
 		this.clientBase.informMove(playerId, position);
+		
+	}
+
+	@Override
+	public void informNewPlayer(int playerId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void informPlayerLeft(int playerId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getPlayerPiecePosition(int playerID) {
+		try {
+			return this.server.getPlayerPiecePosition(playerID);
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return 0;
+	}
+
+	@Override
+	public int addPlayer(IClient client) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setPlayerReady(int player) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getPlayerName(int player) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getPlayerColor(int player) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getTurnsInPrison(int playerID) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Rules getRules() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getEstateName(int position) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getEstateColorGroup(int position) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getEstateHouses(int position) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getEstatePrice(int position) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getEstateHousePrice(int position) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getEstateRent(int position, int houses) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getGameStatusMessage(int playerID) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isMortgaged(int position) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int getOwner(int position) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getDiceValue() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int[] getDiceValues() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getPlayerCash(int playerID) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getPlayerOnTurn() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getNumberOfGetOutOfJailCards(int playerID) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getNumberOfHousesLeft() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getNumberOfHotelsLeft() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean rollDice(int playerID) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean accept(int playerID) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean decline(int playerID) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean endTurn(int playerID) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean useGetOutOfJailCard(int playerID) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean payFine(int playerID) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean declareBankruptcy(int playerID) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean construct(int playerID, int position) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deconstruct(int playerID, int position) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean toggleMortgage(int playerID, int position) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void sendMessage(String text, int sender) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sendPrivateMessage(String text, int sender, int reciever) {
+		// TODO Auto-generated method stub
 		
 	}
 
